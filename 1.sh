6c875b2d18e59e4a4e588123d61ef94ee2921114
moya2667 <moya2667@gmail.com>
2021-09-12 15:18:03 +0900

    intial shell script

---
+++ 1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+var="hello world"
+
+echo "nice to meet you"
+
+echo "$var"
---
+++ args.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Input args count : $#"  #$# 으로 받는구나
+echo "Input args 1 : $1" 
+echo "Input args 2 : $2"
